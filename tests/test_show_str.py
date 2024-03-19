77b26c04b7b40359a2ccd53fa04d66095445d6bd
zv0r <mount.dev.brain@gmail.com>
2024-03-19 20:34:08 +0300

    Copy sample db before testing

--- tests/test_show_str.py
+++ tests/test_show_str.py
@@ -1,12 +1,17 @@
 from subprocess import run, PIPE
+import shutil
 import os
 import pytest
 
 B_FILE_PATH = './bin/show_str'
+DATA_SAMPLES_DIR = './data-samples'
+SAMPLE_DB_FILE_NAME = 'task007.db'
+SAMPLE_DB_FILE_COPY = './tests/task007.db'
 
 if os.path.isfile(B_FILE_PATH):
 
     def test_show_str_1():
+        shutil.copy(SAMPLE_DB_FILE_COPY, os.path.join(DATA_SAMPLES_DIR, SAMPLE_DB_FILE_NAME))
         result = run([B_FILE_PATH], encoding='utf-8', stdout=PIPE)
         assert result.returncode == 0
         assert result.stdout.strip() == '7 Alexandr Kuricin 18 a.kuricin@yssu.ru'
